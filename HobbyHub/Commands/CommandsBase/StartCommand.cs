591eef40d6d432048292c3a9f9e795d0a35bfa45
DigLik <125634228653a@gmail.com>
2024-09-13 02:30:46 +0500

    Chore: Added docker_compose

--- HobbyHub/Commands/CommandsBase/StartCommand.cs
+++ HobbyHub/Commands/CommandsBase/StartCommand.cs
@@ -8,7 +8,7 @@ namespace HobbyHub.Commands.CommandsBase
     {
         public async Task ExecuteAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Привет чертила.", cancellationToken: cancellationToken);
+            await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Привет чертилы.", cancellationToken: cancellationToken);
         }
     }
 }
